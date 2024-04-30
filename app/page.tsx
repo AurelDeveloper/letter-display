199a69f64fcf92e958dcf79c7d4074cef871d983
AurelDeveloper <marlon.profils@gmail.com>
2024-05-01 00:05:46 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import RootLayout from './layout'
 import Posts from '@/app/posts/page'
-import { Theme } from '@radix-ui/themes';
 
 export default function Page() {
     return (
         <RootLayout>
-            <Theme>
                 <Posts/>
-            </Theme>
         </RootLayout>
     )
 }

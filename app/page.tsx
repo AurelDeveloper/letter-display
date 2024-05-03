815e3dd416ef29adcf51ef0296c676fdca1b6fd7
AurelDeveloper <marlon.profils@gmail.com>
2024-05-03 17:08:48 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
     return (
         <RootLayout>
             <h1 className="text-center text-4xl font-black my-4">Newslatter Name</h1>
-            <p className="text-center text-xl mb-8">Short newslatetr description</p>
+            <p className="text-center text-xl mb-8">Short newslatter description</p>
             <FeedCards />
         </RootLayout>
     )
